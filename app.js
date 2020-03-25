8dca522051b040045f1417ba7c11b3a7710a1cc0
manangupta12 <manangupta852@gmail.com>
2020-03-26 01:48:48 +0530

    UI improvements and Clean Code

--- app.js
+++ app.js
@@ -14,27 +14,27 @@ var personalRoutes = require("./routes/personal"),
     indexRoutes = require("./routes/index");
 //cleanDb();
 
-//const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-// replace the uri string with your connection string.
-//const uri = "mongodb+srv://manan:<ATLAS cluster password here>@cluster0-hjcqz.mongodb.net/test?retryWrites=true&w=majority"
-// MongoClient.connect(uri, function(err, client) {
-//    if(err) {
-//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-//    }
-//    console.log('Connected to MongoDB...');
-//    const collection = client.db("test").collection("devices");
-//    // perform actions on the collection object
-//    client.close();
-// });
+//replace the uri string with your connection string.
+const uri = "mongodb+srv://manan:Manangupta852@@cluster0-hjcqz.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected to MongoDB...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
 
-//mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect("mongodb://localhost/test_app",{useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+// mongoose.connect("mongodb://localhost/test_app",{useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
 
 var app = express();
 
